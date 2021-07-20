6492a845c3c7e096626a7cefe70c9786340542f9
alberto <albertozanola1996@gmail.com>
2021-07-20 13:57:12 +0200

    Stampa array con aggiunta cognome

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@ var cognome_utente = prompt("Inserisci il tuo cognome");
 console.log("Il cognome inserito è: " + cognome_utente);
 
 // 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+var cognomi_arrey = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+console.log("I cognomi dell' array sono: " + cognomi_arrey);
+
+cognomi_arrey.push(cognome_utente);
+console.log("i cognomi dell' array più il tuo sono: " + cognomi_arrey);
+
 
 
 
