8cf019afc02509812cd8655b05c063a61e74f819
alberto <albertozanola1996@gmail.com>
2021-07-20 13:43:57 +0200

    Inserire il  cognome

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // 1. chiedi all’utente il cognome;
-
-
+var cognome_utente = prompt("Inserisci il tuo cognome");
+console.log("Il cognome inserito è: " + cognome_utente);
 
 // 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
 
